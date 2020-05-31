00b60f2b53d472bcae5b46a2d5e8987c8ee83fe6
Hyeonmin Yoo <hyeonmin.yoo@gmail.com>
2020-05-31 09:17:43 +0900

    Initial for Lesson 10 CountFactors

---
+++ Lesson10 CountFactors.playground/Contents.swift
@@ -0,0 +1,19 @@
+/*:
+# [Codility Lesson 10 Prime and composite numbers: CountFactors](https://app.codility.com/programmers/lessons/10-prime_and_composite_numbers/count_factors/)
+*/
+/*:
+### The result will be:
+* Task Score		100%
+* Correctness		100%
+* Performance		100%
+*/
+import Foundation
+
+// Given Vriables
+
+public func solution(_ N : Int) -> Int {
+	return 0
+}
+
+// Test
+print()

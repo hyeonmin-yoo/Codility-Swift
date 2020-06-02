374d18b500f62dd363470968b053579e30a11817
Hyeonmin Yoo <hyeonmin.yoo@gmail.com>
2020-06-02 10:02:53 +0900

    Initial Lesson10 Prime and composite numbers: MinPerimeterRectangle

---
+++ Lesson10 MinPerimeterRectangle.playground/Contents.swift
@@ -0,0 +1,22 @@
+/*:
+# [Codility Lesson 10 Prime and composite numbers: MinPerimeterRectangle](https://app.codility.com/programmers/lessons/10-prime_and_composite_numbers/min_perimeter_rectangle/)
+
+## Key Point:
+
+### The result will be:
+* Task Score		100%
+* Correctness		100%
+* Performance		100%
+*/
+
+import Foundation
+
+// Given Vriables
+
+public func solution(_ N : Int) -> Int {
+	
+	return 0
+}
+
+// Test
+print()

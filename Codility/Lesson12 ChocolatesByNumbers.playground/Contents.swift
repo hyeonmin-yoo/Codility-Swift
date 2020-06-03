797846ebe39e901f19d8e275c2e23f2d93d58851
Hyeonmin Yoo <hyeonmin.yoo@gmail.com>
2020-06-03 16:44:26 +0900

    Added solution code

--- Codility/Lesson12 ChocolatesByNumbers.playground/Contents.swift
+++ Codility/Lesson12 ChocolatesByNumbers.playground/Contents.swift
@@ -3,6 +3,9 @@
 
 ## Level: Painless
 
+## Key Point:
+* [Euclidean algorithm](https://en.wikipedia.org/wiki/Euclidean_algorithm#:~:text=In%20mathematics%2C%20the%20Euclidean%20algorithm,them%20both%20without%20a%20remainder.)
+
 ### The result will be:
 * Task Score		100%
 * Correctness		100%
@@ -12,11 +15,20 @@
 import Foundation
 
 // Given Vriables
-
+var numberOfChocolates = 10
+var distance = 4
 
 public func solution(_ N : Int, _ M : Int) -> Int {
-	return 0
+	
+	func gcd(a: Int, b: Int) -> Int {
+		if a % b == 0 {
+			return b
+		} else {
+			return gcd(a: b, b: a % b)
+		}
+	}
+	return N / gcd(a: N, b: M)
 }
 
 // Test
-print()
+print(solution(numberOfChocolates, distance))

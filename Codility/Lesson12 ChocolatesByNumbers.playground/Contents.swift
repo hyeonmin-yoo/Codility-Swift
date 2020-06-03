e3fed0cc5cd38a9a1ca66d95cbb132adf736ba70
Hyeonmin Yoo <hyeonmin.yoo@gmail.com>
2020-06-03 14:59:53 +0900

    Initial Lesson12 ChocolatesByNumbers

---
+++ Codility/Lesson12 ChocolatesByNumbers.playground/Contents.swift
@@ -0,0 +1,22 @@
+/*:
+# [Codility Lesson 12 Euclidean algorithm: ChocolatesByNumbers](https://app.codility.com/programmers/lessons/12-euclidean_algorithm/chocolates_by_numbers/)
+
+## Level: Painless
+
+### The result will be:
+* Task Score		100%
+* Correctness		100%
+* Performance		100%
+*/
+
+import Foundation
+
+// Given Vriables
+
+
+public func solution(_ N : Int, _ M : Int) -> Int {
+	return 0
+}
+
+// Test
+print()

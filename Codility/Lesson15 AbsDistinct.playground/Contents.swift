ef1be4a517f0217e2f1eb84a72c95a6065a9665e
Hyeonmin Yoo <hyeonmin.yoo@gmail.com>
2020-06-04 21:30:20 +0900

    Initial Lesson 15 AbsDistinct

---
+++ Codility/Lesson15 AbsDistinct.playground/Contents.swift
@@ -0,0 +1,26 @@
+/*:
+# [Codility Lesson 15 Caterpillar method: ChocolatesByNumbers](https://app.codility.com/programmers/lessons/15-caterpillar_method/abs_distinct/)
+
+## Level: Painless
+
+## Key Point:
+
+
+### The result will be:
+* Task Score		100%
+* Correctness		100%
+* Performance		100%
+*/
+
+import Foundation
+
+// Given Vriables
+
+
+public func solution(_ A : inout [Int]) -> Int {
+	
+	return 0
+}
+
+// Test
+print()

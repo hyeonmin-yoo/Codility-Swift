60a2f1a3edea362504a28d67079f48ab3a0e3a31
Hyeonmin Yoo <hyeonmin.yoo@gmail.com>
2020-06-05 22:37:54 +0900

    Initial Lesson15 CountDistinctSlices

---
+++ Codility/Lesson15 CountDistinctSlices.playground/Contents.swift
@@ -0,0 +1,23 @@
+/*:
+# [Codility Lesson 15 Caterpillar method: CountDistinctSlices](https://app.codility.com/programmers/lessons/15-caterpillar_method/count_distinct_slices/)
+
+## Level: Painless
+
+### The result will be:
+* Task Score		100%
+* Correctness		100%
+* Performance		100%
+*/
+
+import Foundation
+
+// Given Vriables
+
+
+public func solution(_ M : Int, _ A : inout [Int]) -> Int {
+	
+	return 0
+}
+
+// Test
+print()

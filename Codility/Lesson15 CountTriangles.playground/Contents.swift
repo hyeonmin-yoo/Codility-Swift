401b06ad07c83cc0af8e3a30dc2ddfe343d85d33
Hyeonmin Yoo <hyeonmin.yoo@gmail.com>
2020-06-15 09:54:22 +0900

    Initial commit for lesson 15  CountTriangles

---
+++ Codility/Lesson15 CountTriangles.playground/Contents.swift
@@ -0,0 +1,26 @@
+/*:
+# [Codility Lesson 15 Caterpillar method: CountTriangles](https://app.codility.com/programmers/lessons/15-caterpillar_method/count_triangles/)
+
+## Level: Painless
+
+## Key Points:
+* Two-Pointer Algorithm
+
+### The result will be:
+* Task Score		100%
+* Correctness		100%
+* Performance		100%
+*/
+
+import Foundation
+
+// Given Vriables
+
+
+public func solution(_ A : inout [Int]) -> Int {
+	
+	return 0
+}
+
+// Test
+print()

01644c3a4f165d46ec69649b5a6c8306b1c14620
Hyeonmin Yoo <hyeonmin.yoo@gmail.com>
2020-06-17 10:12:59 +0900

    Lesson 16 MaxNonoverlappingSegments initialized

---
+++ Codility/Lesson16 MaxNonoverlappingSegments.playground/Contents.swift
@@ -0,0 +1,28 @@
+/*:
+# [Codility Lesson 16
+Greedy algorithms:
+MaxNonoverlappingSegments](https://app.codility.com/programmers/lessons/16-greedy_algorithms/max_nonoverlapping_segments/)
+
+## Level: Painless
+
+
+## Key Points:
+
+
+### The result will be:
+* Task Score		100%
+* Correctness		100%
+* Performance		100%
+*/
+
+import Foundation
+
+// Given Vriables
+
+public func solution(_ A : inout [Int], _ B : inout [Int]) -> Int {
+	
+	return 0
+}
+
+// Test
+print()

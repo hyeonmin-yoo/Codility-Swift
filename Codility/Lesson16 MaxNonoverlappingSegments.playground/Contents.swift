b46822cc6f33f718b4025dcfd7901402bd824b61
Hyeonmin Yoo <hyeonmin.yoo@gmail.com>
2020-06-17 13:58:36 +0900

    Added solution for Lesson 16 Max Nonoverlapping Segments

--- Codility/Lesson16 MaxNonoverlappingSegments.playground/Contents.swift
+++ Codility/Lesson16 MaxNonoverlappingSegments.playground/Contents.swift
@@ -7,6 +7,7 @@ MaxNonoverlappingSegments](https://app.codility.com/programmers/lessons/16-greed
 
 
 ## Key Points:
+* [Greedy Algorithm](https://ko.wikipedia.org/wiki/%ED%83%90%EC%9A%95_%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98)
 
 
 ### The result will be:
@@ -18,11 +19,23 @@ MaxNonoverlappingSegments](https://app.codility.com/programmers/lessons/16-greed
 import Foundation
 
 // Given Vriables
+var givenArray1 = [1, 3, 7, 9, 9]
+var givenArray2 = [5, 6, 8, 9, 10]
 
 public func solution(_ A : inout [Int], _ B : inout [Int]) -> Int {
 	
-	return 0
+	var maxNonOverlapping = 0
+	let countA = A.count
+	var end = -1
+	
+	for index in 0..<countA {
+		if A[index] > end {
+			maxNonOverlapping += 1
+			end = B[index]
+		}
+	}
+	return maxNonOverlapping
 }
 
 // Test
-print()
+print(solution(&givenArray1, &givenArray2))

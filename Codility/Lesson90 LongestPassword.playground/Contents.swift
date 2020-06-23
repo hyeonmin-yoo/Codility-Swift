d5c318ee38ec1daaf3531b8b60386348c2c1501b
Hyeonmin Yoo <hyeonmin.yoo@gmail.com>
2020-06-23 12:40:55 +0900

    Initial commit for lesson 90 LongestPassword

---
+++ Codility/Lesson90 LongestPassword.playground/Contents.swift
@@ -0,0 +1,26 @@
+/*:
+# [Codility Lesson 90
+Tasks from Indeed Prime 2015 challenge:
+LongestPassword](https://app.codility.com/programmers/lessons/90-tasks_from_indeed_prime_2015_challenge/longest_password/)
+
+## Level: Painless
+
+
+### The result will be:
+* Task Score		100%
+* Correctness		100%
+* Performance		100%
+*/
+
+import Foundation
+
+// Given Vriables
+
+public func solution(_ S : inout String) -> Int {
+	
+	return 0
+}
+
+
+// Test
+print()

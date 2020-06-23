e1a3c883e18f2f11741ca3cbfcc1c0e90c8fdaa7
Hyeonmin Yoo <hyeonmin.yoo@gmail.com>
2020-06-23 11:52:06 +0900

    Initial commit for lesson 16 TieRopes

---
+++ Codility/Lesson16 TieRopes.playground/Contents.swift
@@ -0,0 +1,27 @@
+/*:
+# [Codility Lesson 16
+Greedy algorithms:
+TieRopes](https://app.codility.com/programmers/lessons/16-greedy_algorithms/tie_ropes/)
+
+## Level: Painless
+
+
+## Key Points:
+* [Greedy Algorithm](https://ko.wikipedia.org/wiki/%ED%83%90%EC%9A%95_%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98)
+
+
+### The result will be:
+* Task Score		100%
+* Correctness		100%
+* Performance		100%
+*/
+
+import Foundation
+
+// Given Vriables
+public func solution(_ K : Int, _ A : inout [Int]) -> Int {
+	return 0
+}
+
+// Test
+print()
